292d22a93e91c6b3232b8f06c6536af69ef52325
unknown <cispal19@gmail.com>
2019-03-14 20:28:01 -0500

    cambios

---
+++ server/index.js
@@ -0,0 +1,38 @@
+'use strict'
+ var express = require('express');
+ var app = express();
+ var server = require('http').Server(app);
+ var io = require('socket.io')(server);
+
+ app.use(express.static('client'));
+
+app.get('/hola-mundo',function(req,res){
+    res.status(200).send('hola mundo');
+});
+
+var messages = [{
+    id:1,
+    text:'Bienvenido al chat privado',
+    nickname:'Bot - julio'
+}];
+
+io.on('connection', (socket) =>{
+    console.log('El nodo con IP: '+socket.handshake.address + 'se ha conectado...');
+    socket.emit('messages',messages);
+
+    socket.on('add-message',function(data){
+        messages.push(data);
+
+        io.sockets.emit('messages',messages);
+
+    });
+
+
+
+
+});
+
+ server.listen(6677,function(){
+     console.log('servidor esta funcionando en http://localhost:6677');
+
+ });
